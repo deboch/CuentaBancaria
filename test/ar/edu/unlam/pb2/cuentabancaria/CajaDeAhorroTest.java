961e1290db65c1fa060b947d70910082ce9998e0
Usuario <Usuario@NETBOOK>
2017-05-11 11:37:33 -0300

    "Test realizados"

--- test/ar/edu/unlam/pb2/cuentabancaria/CajaDeAhorroTest.java
+++ test/ar/edu/unlam/pb2/cuentabancaria/CajaDeAhorroTest.java
@@ -1,5 +1,45 @@
 package ar.edu.unlam.pb2.cuentabancaria;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CajaDeAhorroTest {
 
+	@Test
+	public void testQueExtraigaDineroSinSuperarLasCincoVeces() {
+
+		CajaDeAhorro miCuenta = new CajaDeAhorro();
+		miCuenta.depositarDinero(5000.0);
+		miCuenta.extraerD(1000.0);
+
+		miCuenta.extraerD(1000.0);
+
+		Double valorEsperado = 3000.0;
+
+		Double ValorObtenido = miCuenta.getSaldo();
+
+		Assert.assertEquals(valorEsperado, ValorObtenido);
+	}
+
+	@Test
+	public void testQueExtraigaDineroSuperandoLasCincoVeces() {
+		CajaDeAhorro miCuenta = new CajaDeAhorro();
+
+		miCuenta.depositarDinero(8000.0);
+
+		miCuenta.extraerD(1000.0);
+		miCuenta.extraerD(1000.0);
+		miCuenta.extraerD(1000.0);
+		miCuenta.extraerD(1000.0);
+		miCuenta.extraerD(1000.0);
+		miCuenta.extraerD(1000.0);
+		miCuenta.extraerD(1000.0);
+
+		Double valorEsperado = 988.0;
+
+		Double ValorObtenido = miCuenta.getSaldo();
+
+		Assert.assertEquals(valorEsperado, ValorObtenido);
+
+	}
 }
--- test/ar/edu/unlam/pb2/cuentabancaria/CuentaTest.java
+++ test/ar/edu/unlam/pb2/cuentabancaria/CuentaTest.java
@@ -1,5 +1,41 @@
 package ar.edu.unlam.pb2.cuentabancaria;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CuentaTest {
 
+	@Test
+
+	public void testQueDepositeSaldoCorrecto() {
+
+		Cuenta miCuenta = new Cuenta();
+
+		Boolean valorEsperado = true;
+		Assert.assertEquals(valorEsperado, miCuenta.depositarDinero(500.0));
+
+		Boolean valorEsperado2 = false;
+		Assert.assertEquals(valorEsperado2, miCuenta.depositarDinero(-500.0));
+
+	}
+
+	@Test
+
+	public void testQueExtraigaCorrectamente() {
+		Cuenta miCuenta = new Cuenta();
+		miCuenta.depositarDinero(5000.0);
+		Assert.assertTrue(miCuenta.extraerDinero(500.0));
+		Assert.assertFalse(miCuenta.extraerDinero(6000.0));
+
+	}
+
+	@Test
+
+	public void testQueConsulteSaldo() {
+		Cuenta miCuenta = new Cuenta();
+
+		miCuenta.depositarDinero(100.0);
+		Assert.assertEquals((Double) 100.0, miCuenta.getSaldo());
+
+	}
 }
